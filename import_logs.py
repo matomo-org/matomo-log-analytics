92eb866f54f0918b918446802c46e894b0801752
Marcello Nuccio <marcello.nuccio@gmail.com>
2015-06-24 16:11:25 +0200

    Add option for date format in logs.
    Custom log format can be specified with --log-format-regex option.
    But there's no way to also specify a format string for the date.

--- import_logs.py
+++ import_logs.py
@@ -563,6 +563,10 @@ class Configuration(object):
                  "Overrides --log-format-name." % (', '.join(available_regex_groups))
         )
         option_parser.add_option(
+            '--log-date-format', dest='log_date_format', default=None,
+            help="Format string used to parse dates."
+        )
+        option_parser.add_option(
             '--log-hostname', dest='log_hostname', default=None,
             help="Force this hostname for a log format that doesn't include it. All hits "
             "will seem to come to this host"
@@ -762,7 +766,7 @@ class Configuration(object):
             logging.debug('Accepted hostnames: all')
 
         if self.options.log_format_regex:
-            self.format = RegexFormat('custom', self.options.log_format_regex)
+            self.format = RegexFormat('custom', self.options.log_format_regex, self.options.log_date_format)
         elif self.options.log_format_name:
             try:
                 self.format = FORMATS[self.options.log_format_name]
