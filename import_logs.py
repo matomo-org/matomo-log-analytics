matomo-org/matomo-log-analytics

commit 0b38c5d6bc84dbd36ed90a8f51a792997c4a9fd4

Add --accept-invalid-ssl-certificate option. (#199)
This option allows a user to ignore certificate validation problems when
connecting to the Piwik server. This is useful when the server uses a
self-signed or otherwise invalid certificate, but obviously should only
be used when the connection can be trusted.

import_logs.py
@@ -36,6 +36,8 @@ import os
 import os.path
 import Queue
 import re
+import ssl
+import sys
 import threading
 import time
 import urllib
@@ -826,6 +828,13 @@ class Configuration(object):
             '--exclude-newer-than', action='callback', type='string', default=None, callback=functools.partial(self._set_date, 'exclude_newer_than'),
             help="Ignore logs newer than the specified date. Exclusive. Date format must be YYYY-MM-DD hh:mm:ss +/-0000. The timezone offset is required."
         )
+
+        option_parser.add_option(
+            '--accept-invalid-ssl-certificate',
+            dest='accept_invalid_ssl_certificate', action='store_true',
+            default=False,
+            help="Do not verify the SSL / TLS certificate when contacting the Piwik server."
+        )
         return option_parser
 
     def _set_date(self, option_attr_name, option, opt_str, value, parser):
@@ -1369,7 +1378,16 @@ class Piwik(object):
             base64string = base64.encodestring('%s:%s' % (auth_user, auth_password)).replace('\n', '')
             request.add_header("Authorization", "Basic %s" % base64string)        
 
-        opener = urllib2.build_opener(Piwik.RedirectHandlerWithLogging())
+        # Use non-default SSL context if invalid certificates shall be
+        # accepted.
+        if config.options.accept_invalid_ssl_certificate:
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+        else:
+            ssl_context = None
+        opener = urllib2.build_opener(Piwik.RedirectHandlerWithLogging(),
+                                      urllib2.HTTPSHandler(context=ssl_context))
         response = opener.open(request, timeout = timeout)
         result = response.read()
         response.close()

commit 59cf5a36bd91e82681a50d13e206df67bc17d91a

Make code compatible with Python <=2.7.8.
This fixes a regression introduced with PR #199 (commit a93a916e6efd).
The "context" parameter of the urllib2.HTTPSHandler constructor was only
introduced in Python 2.7.9, so we cannot pass a specific SSL context in older
Python versions. Luckily, we do not have to either because Python before 2.7.9
did not validate remote certificates anyway, so the behavior specified by the
--accept-invalid-ssl-certificate option is actually the default when running
on these versions of Python.

import_logs.py
@@ -833,7 +833,7 @@ class Configuration(object):
             '--accept-invalid-ssl-certificate',
             dest='accept_invalid_ssl_certificate', action='store_true',
             default=False,
-            help="Do not verify the SSL / TLS certificate when contacting the Piwik server."
+            help="Do not verify the SSL / TLS certificate when contacting the Piwik server. This is the default when running on Python 2.7.8 or older."
         )
         return option_parser
 
@@ -1380,14 +1380,17 @@ class Piwik(object):
 
         # Use non-default SSL context if invalid certificates shall be
         # accepted.
-        if config.options.accept_invalid_ssl_certificate:
+        if config.options.accept_invalid_ssl_certificate and \
+                sys.version_info >= (2, 7, 9):
             ssl_context = ssl.create_default_context()
             ssl_context.check_hostname = False
             ssl_context.verify_mode = ssl.CERT_NONE
+            https_handler_args = {'context': ssl_context}
         else:
-            ssl_context = None
-        opener = urllib2.build_opener(Piwik.RedirectHandlerWithLogging(),
-                                      urllib2.HTTPSHandler(context=ssl_context))
+            https_handler_args = {}
+        opener = urllib2.build_opener(
+            Piwik.RedirectHandlerWithLogging(),
+            urllib2.HTTPSHandler(**https_handler_args))
         response = opener.open(request, timeout = timeout)
         result = response.read()
         response.close()