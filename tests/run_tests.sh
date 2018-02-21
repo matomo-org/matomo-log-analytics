ab2ed8d73f000d707cc7f33d52ab2120d5e8a514
Lukas Winkler <l.winkler23@mailbox.org>
2018-02-21 20:44:48 +0100

    replace warning

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)
 
 # Make sure nosetests is installed.
-pytest --version  >/dev/null 2>&1 || (echo "nose (http://readthedocs.org/docs/nose/en/latest/) must be installed"; exit 1)
+pytest --version  >/dev/null 2>&1 || (echo "pytest (https://docs.pytest.org/en/latest/getting-started.html) must be installed"; exit 1)
 
 PYTHONPATH=.. pytest $*
