8468161de6553b8703e7f317e53040f8a46cb28c
Lukas Winkler <git@lw1.at>
2019-09-13 11:27:03 +0200

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -915,9 +915,9 @@ def test_matomo_error_construct():
     """Test that Matomo exception can be created."""
 
     try:
-        raise import_logs.Matomo.Error('test message', 120)
+        raise import_logs.MatomoHttpBase.Error('test message', 120)
 
         assert false
-    except import_logs.Matomo.Error as e:
+    except import_logs.MatomoHttpBase.Error as e:
         assert e.code == 120
         assert e.args[0] == 'test message'
